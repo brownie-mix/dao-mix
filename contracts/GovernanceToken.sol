71c464fa5c2c581a7b4590726902e6c22db05b81
Azlan Aziz <azizazlan@gmail.com>
2022-03-17 13:36:01 +0800

    Changed total supply of token from 1000000 to 100 just to check the impact of quorum

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -5,7 +5,7 @@ import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol';
 
 contract GovernanceToken is ERC20Votes {
   // uint256 public s_maxSupply = 1000000000000000000000000;
-  uint256 public s_maxSupply = 100000000000000000000;
+  uint256 public s_maxSupply = 100000000000000000000; // smaller to check the quorum
 
   constructor() ERC20('GovernanceToken', 'GT') ERC20Permit('GovernanceToken') {
     _mint(msg.sender, s_maxSupply);
