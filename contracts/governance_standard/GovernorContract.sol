3159ef7d4d33265288ab0aa12629611d0d66874d
Shuang Song <6882971+ssghost@users.noreply.github.com>
2022-04-15 13:29:09 +0800

    add import ERC20Votes

--- contracts/governance_standard/GovernorContract.sol
+++ contracts/governance_standard/GovernorContract.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract GovernorContract is
     Governor,
